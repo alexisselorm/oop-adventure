13cc1e7f45fc2fc15175649dd3eac4e51c10cd40
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-03-24 13:16:16 +0200

    Inheritance, Extending classes and contructors

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPAdventure;
+
+Console.WriteLine($"Hello, what is your name?");
+
+string? name = Console.ReadLine();
+
+if (name ==String.Empty)
+{
+    name = "Rabid Dog";
+}
+
+Player player = new Player(name);
+
+// do
+// {
+//     Console.WriteLine($"Seriously dude, what is your name?");
+//     name = Console.ReadLine();
+    
+// } while (name == String.Empty);
+
+Console.WriteLine($"Hello {player.Name}");
+
---
+++ src/Player.cs
@@ -0,0 +1,5 @@
+﻿namespace OOPAdventure;
+
+public class Player: Character {
+	public Player(string name) : base(name) { }
+}
